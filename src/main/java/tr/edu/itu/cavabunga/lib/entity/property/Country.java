74fa012c4c0bac6999e27cf80c2cacf7f76b97f9
Semih Serhat Karakaya <karakayasemi@itu.edu.tr>
2018-10-03 17:30:34 +0300

    fix some code smells

--- src/main/java/tr/edu/itu/cavabunga/lib/entity/property/Country.java
+++ src/main/java/tr/edu/itu/cavabunga/lib/entity/property/Country.java
@@ -6,9 +6,4 @@ import javax.persistence.Entity;
 
 @Entity
 public class Country extends Property {
-    //TODO: check rfc5545 to confirm if it exists
-    @Override
-    public void validate(){
-
-    }
 }
